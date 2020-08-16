7b255c7f8c7fe8472178f1e12f670eb4c81c897d
Simon Zeng <zeng.simonl@gmail.com>
2020-08-15 22:07:12 -0400

    Added title to DashboardScreen

--- screens/DashboardScreen.js
+++ screens/DashboardScreen.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import { View, Text } from 'react-native';
 
 export default class DashboardScreen extends Component {
+	static navigationOptions = {
+		title : 'Home'
+	};
+
 	constructor(props) {
 		super(props);
 
