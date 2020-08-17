b52570ebbed11293e2960c10d96e29c0b909f08f
Jake <jakejaekwak@gmail.com>
2020-08-16 21:51:31 -0400

    fixed searchbar state issue

--- screens/SearchScreen.js
+++ screens/SearchScreen.js
@@ -1,28 +1,29 @@
 import React from 'react';
+import { View, Text } from 'react-native';
 import { SearchBar } from 'react-native-elements';
-
 class SearchScreen extends React.Component {
 	static navigationOptions = {
 		title : 'Search'
 	};
 	constructor(props) {
 		super(props);
-
 		this.state = {
 			company : ''
 		};
 	}
 
-	updateCompany = (companyName) => {
-		this.setState({ company: companyName });
+	updateCompany = (company) => {
+		this.setState({ company });
 	};
-
 	render() {
+
 		const { company } = this.state;
-		return (
+		const searchBar = (
 			<SearchBar platform="ios" placeholder="Company Name..." onChangeText={this.updateCompany} value={company} />
 		);
+
+		console.log('this company', {company})
+		return (searchBar)
 	}
 }
-
 export default SearchScreen;
